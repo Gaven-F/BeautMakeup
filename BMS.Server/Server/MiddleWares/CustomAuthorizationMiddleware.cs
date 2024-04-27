2af01cb0ae26b2e77e2016cbc6b8c4022bad8109
fallen gaven <gavenf2023@outlook.com>
2024-04-27 19:53:12 +0800

    BACKUP

---
+++ BMS.Server/Server/MiddleWares/CustomAuthorizationMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Masuit.Tools.Mime;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+
+namespace Server.MiddleWares;
+
+public class CustomAuthorizationMiddleware(RequestDelegate next)
+{
+
+	public async Task InvokeAsync(HttpContext context)
+	{
+		// 进行自定义的授权逻辑
+		if (context.User.Identities == null || !context.User.Identity!.IsAuthenticated)
+		{
+			// 授权失败时返回自定义的响应
+			context.Response.StatusCode = StatusCodes.Status403Forbidden;
+			context.Response.ContentType = MimeMapper.MimeTypes[".json"];
+			await context.Response.WriteAsync("{\"message\": \"授权失败\"}");
+			return;
+		}
+
+		// 授权通过，继续处理请求
+		await next(context);
+	}
+}
+
+public class Demo : IAuthorizationMiddlewareResultHandler
+{
+
+	public Task HandleAsync(RequestDelegate next, HttpContext context, AuthorizationPolicy policy, PolicyAuthorizationResult authorizeResult)
+	{
+		Console.WriteLine("ENTER");
+		if (authorizeResult.Forbidden)
+		{
+			Console.WriteLine("FORBIDDEN");
+		}
+		return Task.CompletedTask;
+	}
+}
